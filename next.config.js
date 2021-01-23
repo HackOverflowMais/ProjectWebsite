0737a58ff16e49c9eb8d29859f7fe6ba99a0b330
Daggy1234 <arnav.jindal7@gmail.com>
2021-01-23 15:32:37 +0530

    Inital Bootstrap

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withSass = require('@zeit/next-sass');
+const withPWA = require('next-pwa');
+
+module.exports = withPWA(
+    withSass({
+        cssModules: true,
+        webpack: (config, { isServer }) => {
+            if (isServer) {
+                require('./lib/generate-sitemap');
+            }
+
+            return config;
+        }
+    })
+);
