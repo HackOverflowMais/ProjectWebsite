89a7fa357a1f6c5dc2a8daddaacbe4731bacef08
Daggy1234 <arnav.jindal7@gmail.com>
2021-01-23 15:35:50 +0530

    Fixes

--- next.config.js
+++ next.config.js
@@ -2,15 +2,13 @@
 const withSass = require('@zeit/next-sass');
 const withPWA = require('next-pwa');
 
-module.exports = withPWA(
-    withSass({
-        cssModules: true,
-        webpack: (config, { isServer }) => {
-            if (isServer) {
-                require('./lib/generate-sitemap');
-            }
-
-            return config;
+module.exports = withSass({
+    cssModules: true,
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            require('./lib/generate-sitemap');
         }
-    })
-);
+
+        return config;
+    }
+});
