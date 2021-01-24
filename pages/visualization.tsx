7664c76a543de4e46e3e4c967ffdcebc5f77f552
Daggy1234 <arnav.jindal7@gmail.com>
2021-01-24 11:31:44 +0530

    Data

--- pages/visualization.tsx
+++ pages/visualization.tsx
@@ -12,26 +12,26 @@ export default function Visualization() {
                     title="Visualization"
                     src="https://hackoverflowmais.github.io/simulate-covid/"
                     width="100%"
-                    height="500px"
+                    height="1000px"
                 />
             </Box>
-            <Flex mt={10} direction={{ base: 'column', md: 'row' }}>
-                <Flex direction="column">
-                    <Heading size="lg">Countiries with teh Most covid cases</Heading>
+            <Flex mt={10} m={10} direction={{ base: 'column', md: 'row' }}>
+                <Flex margin={5} direction="column">
+                    <Heading size="md">Countries with the Most covid cases</Heading>
                     <Image
                         src="/newplot.png"
                         alt="top 10 confirmed cases"
-                        height={300}
-                        width={300}
+                        height={400}
+                        width={400}
                     />
                 </Flex>
-                <Flex direction="column">
-                    <Heading size="lg">States in India with most covid cases</Heading>
+                <Flex direction="column" margin={5}>
+                    <Heading size="md">States in India with most covid cases</Heading>
                     <Image
                         src="/plot2.png"
                         alt="top 10 confirmed case by state Indias"
-                        height={300}
-                        width={300}
+                        height={400}
+                        width={400}
                     />
                 </Flex>
             </Flex>
