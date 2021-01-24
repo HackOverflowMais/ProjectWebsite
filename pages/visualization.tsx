631d11eb5e72dea75a2ce5a3b925e705d60a3081
Daggy1234 <arnav.jindal7@gmail.com>
2021-01-24 11:25:55 +0530

    new plots

--- pages/visualization.tsx
+++ pages/visualization.tsx
@@ -1,10 +1,12 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Heading } from '@chakra-ui/react';
+import Image from 'next/image';
 
 import Layout from '../components/layout';
 
 export default function Visualization() {
     return (
         <Layout>
+            <Heading textAlign="center">Covid Visualized</Heading>
             <Box paddingTop="20%">
                 <iframe
                     title="Visualization"
@@ -13,6 +15,27 @@ export default function Visualization() {
                     height="500px"
                 />
             </Box>
+            <Flex mt={10} direction={{ base: 'column', md: 'row' }}>
+                <Flex direction="column">
+                    <Heading size="lg">Countiries with teh Most covid cases</Heading>
+                    <Image
+                        src="/newplot.png"
+                        alt="top 10 confirmed cases"
+                        height={300}
+                        width={300}
+                    />
+                </Flex>
+                <Flex direction="column">
+                    <Heading size="lg">States in India with most covid cases</Heading>
+                    <Image
+                        src="/plot2.png"
+                        alt="top 10 confirmed case by state Indias"
+                        height={300}
+                        width={300}
+                    />
+                </Flex>
+            </Flex>
+            <Image />
         </Layout>
     );
 }
